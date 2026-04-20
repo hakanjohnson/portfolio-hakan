957ad9689562a556222bad65dbf4fba286c70ca9
Hakan Johnson <hakan.johnson@aol.de>
2026-04-20 22:35:23 +0200

    design change - first final push

--- main.js
+++
@@ -1,6 +0,0 @@
-fetch("components/navigation.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("placeholder_navigation").innerHTML = data;
-    })
-    .catch(error => console.error("Fehler beim Laden", error));

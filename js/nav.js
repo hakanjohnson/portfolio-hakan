408e359ba1d1d1e13084a46f36b4b0a43943d99c
Hakan Johnson <hakan.johnson@aol.de>
2026-05-04 22:18:50 +0200

    nav changes

--- js/nav.js
+++ js/nav.js
@@ -5,23 +5,25 @@ button.addEventListener("click", function() {
     if (menu.classList.contains("open")) {
         menu.classList.remove("open");
         button.classList.remove("active");
-
+        document.documentElement.classList.remove("no-scroll");
+        document.body.classList.remove("no-scroll");
     }
     else {
         menu.classList.add("open");
         button.classList.add("active");
+        document.documentElement.classList.add("no-scroll");
+        document.body.classList.add("no-scroll");
     }
-})
+});
 
-const subbtn = document.getElementById("menu3");
-const sub = document.getElementById("submenu");
+function closeMenu() {
+    document.body.classList.remove("no-scroll");
+    document.documentElement.classList.remove("no-scroll");
+    menu.classList.remove("open");
+    button.classList.remove("active");
 
-subbtn.addEventListener("click", function() {
-    if(submenu.classList.contains("active")) {
-        submenu.classList.remove("active");
-    }
-    else {
-        submenu.classList.add("active");
-    }
-})
+}
 
+document.querySelectorAll("nav ul li a").forEach( a => {
+    a.addEventListener("click", closeMenu);
+});
